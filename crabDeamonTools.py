b4f7fe0847f0534f045d20d84362cdb79d05f764
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-12-12 00:19:57 +0100

    total number added

--- crabDeamonTools.py
+++ crabDeamonTools.py
@@ -86,7 +86,7 @@ class crabDeamon(object):
           createdJobs.append(jSplit[0])
     if not onlySummary:
       print " downloadableJobs ",downloadableJobs
-      print "doneJobsGood ",doneJobsGood
+      print "doneJobsGood ",doneJobsGood," of ",len(jobOutput)
       print "doneJobsBad ", doneJobsBad 
       print "abortedJobs ",abortedJobs
       print "downloadedJobsBad ",downloadedJobsBad
