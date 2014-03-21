febc167e42ac1d52ca42f83b031f7384da11b581
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-03-22 00:32:16 +0100

    no additional new lines

--- crabDeamonTools.py
+++ crabDeamonTools.py
@@ -28,7 +28,7 @@ class crabDeamon(object):
       if 'returnCodeCrab' in line:
         crabExitCode=line
       if debug:
-        print line
+        print line,
       subPStdOut.append(line)
     subPrOutput.stdout.close()
     crabExitCode = re.match('returnCodeCrab:\ ([^!]*)!',crabExitCode).group(1) if re.match('returnCodeCrab:([^!]*)!',crabExitCode) else None
